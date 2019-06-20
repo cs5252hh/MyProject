52dfb96760e793182b4ef010f355fa46b38f4c74
luciferchang <luciferchang@luciferchangdeMacBook-Pro.local>
2019-06-20 21:53:46 +0800

    Initial commit

---
+++ src/main/java/Alex/com/Hello.java
@@ -0,0 +1,12 @@
+package Alex.com;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        Person p=new Person();
+        p.Hello();
+
+    }
+
+}
---
+++ src/main/java/Alex/com/Person.java
@@ -0,0 +1,10 @@
+package Alex.com;
+
+public class Person {
+    public void Hello(){
+        System.out.println("Hello World");
+
+    }
+
+
+}
