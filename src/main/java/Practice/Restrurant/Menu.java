4a567f9cc79b6e17f1a517a7c90cda44734b9a00
luciferchang <cs525hh@gmail.com>
2019-07-02 23:50:28 +0800

    Practice Order Menu

---
+++ src/main/java/Practice/Restrurant/Menu.java
@@ -0,0 +1,35 @@
+package Practice.Restrurant;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        Order order = new Order(null,null,null);
+        System.out.println("Welcome !");
+        System.out.println("Here are salad:");
+        System.out.println(order.setSalad());
+        System.out.println("Please enter number to order salad:");
+        int saladinput = scanner.nextInt()-1;
+        String selfsalad= (order.setSalad().get(saladinput));
+
+        System.out.println("Here are meal:");
+        System.out.println(order.setMeal());
+        System.out.println("Please enter number to order meal:");
+        int mealinput = scanner.nextInt()-1;
+        String selfmeal= (order.setMeal().get(mealinput));
+
+        System.out.println("Here are drink:");
+        System.out.println(order.setDrink());
+        System.out.println("Please enter number to order drink:");
+        int drinkinput = scanner.nextInt()-1;
+        String selfdrink= (order.setDrink().get(drinkinput));
+
+        System.out.print("Your order are:"+ "\t" + selfsalad+ "\t"+ selfmeal
+                + "\t" + selfdrink);
+    }
+}
